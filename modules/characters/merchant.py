8a6fbb13044d782f7491627a6c3253cc564de08b
Yash <yashrawal987@gmail.com>
2026-04-30 21:22:18 +0530

    specail cases for merchnat

--- modules/characters/merchant.py
+++ modules/characters/merchant.py
@@ -10,29 +10,34 @@ class Merchant(PhysicsEntity):
         self.tile_size = 36
         
         self.load_animations()
-        self.rect = self.animations.get_current_frame().get_rect(topleft=(x, y))
+        # Initialize rect using the position set by PhysicsEntity
+        self.rect = self.animations.get_current_frame().get_rect(topleft=(self.pos.x, self.pos.y))
+        # Shrink height so physics brings the feet all the way to the tile
+        self.rect.height = 30 
 
     def load_animations(self):
-        # Using the base_idle frames provided by the user
-        path = os.path.join("Assets", "PNG", "Characters", "Merchant", "frames", "base_idle")
+        path = os.path.join("Assets", "PNG", "Characters", "Merchant", "frames", "base_idle_styled")
         frames = {'idle': []}
         
-        # Load all 8 frames for idle
         for i in range(1, 9):
             img_path = os.path.join(path, f"{i}.png")
             if os.path.exists(img_path):
                 img = pygame.image.load(img_path).convert_alpha()
-                # Scaling to tile_size to match engine standards
                 frames['idle'].append(pygame.transform.scale(img, (self.tile_size, self.tile_size)))
         
-        self.animations = AnimationManager(frames)
+        # Very slow animation speed
+        self.animation_speed = 0.03
+        self.animations = AnimationManager(frames, animation_speed=self.animation_speed)
         self.animations.change_state('idle')
 
     def update(self, platforms, **kwargs):
-        # Merchant is harmless and stationary, but still needs basic physics (gravity)
         self.apply_physics(platforms)
         self.animations.update()
         self.image = self.animations.get_current_frame()
 
     def draw(self, screen, camera):
-        screen.blit(self.image, camera.apply(self))
+        # Draw with a 6-pixel downward offset to compensate for the 32x32 transparent padding
+        # and ensure the feet touch the ground.
+        pos = camera.apply(self)
+        pos.y += 6
+        screen.blit(self.image, pos)
